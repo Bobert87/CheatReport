a9ee122f4e91874956f024a10ac8cc675c46408a
Bobert <bruiz@healthcare.com>
2016-04-10 18:42:39 -0600

    CheatReport

---
+++ CheatReport/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CheatReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Ingrese folder (ruta completa) ");
+            string folder = Console.ReadLine();
+            Reporter report = new Reporter(folder);
+            report.GetReport();
+            PrettyPrint(report);
+            Console.ReadLine();
+        }
+
+        public static void PrettyPrint(Reporter r)
+        {
+            float copyRatio = 1f -(r.Projects.Count/r.FilesParsed);
+                        
+            r.Projects.OrderBy(x => x.Value.Count);
+            foreach (var project in r.Projects)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(project.Key);
+                int copiedProjects = 0;
+                for (int i = 0; i < ((List<string>)project.Value).Count; i++)                
+                {
+                    if (((List<string>)project.Value).Count == 1)
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    else                    
+                        Console.ForegroundColor = ConsoleColor.Red;                    
+                    Console.WriteLine("\t " + i + ") " + project.Value[i]);
+                }
+            }
+        }
+    }
+}
---
+++ CheatReport/Reporter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CheatReport
+{
+    
+    public class Reporter
+    {
+        public Reporter(string folder)
+        {
+            Folder = folder;
+            Projects = new Dictionary<string, List<string>>();
+        }
+
+        private string Folder{ get; set; }
+        private const string VSEXTENSION = "csproj";
+        public Dictionary<string, List<string>> Projects;
+        public int FilesParsed { get; set; }
+        private void AddProject(string guid, string proj)
+        {
+            if (!Projects.ContainsKey(guid))
+            {
+                Projects.Add(guid,new List<string>() {proj});
+            }
+            else
+            {
+                Projects[guid].Add(proj);
+            }
+        }
+
+        private List<string> LookForProjects(string currentFolder)
+        {
+            DirectoryInfo di = new DirectoryInfo(currentFolder);
+            FileInfo[] projectFilesInFolder = di.GetFiles("*." + VSEXTENSION, SearchOption.AllDirectories);
+            List<string> files = projectFilesInFolder.Select(fileInfo => fileInfo.FullName).ToList();            
+            return files;
+        }
+
+        private string GetGuid(string csProjFile)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(csProjFile);            
+            return xmlDoc.GetElementsByTagName("ProjectGuid")[0].InnerText.Replace("{","").Replace("}","");
+        }
+
+        public void GetReport()
+        {
+            List<string> projectList = LookForProjects(Folder);
+            FilesParsed = projectList.Count;
+            foreach (var p in projectList)
+            {                
+                AddProject(GetGuid(p),p);
+            }
+        }
+    }
+}
